74df6dd6c70ffa016a9c3e1416690edbf32dddc0
Nemanja Spasojevic <nemanja.spasojevic.memi@gmail.com>
2020-10-02 02:17:13 +0200

    products page updated

--- src/public/components/Gallery.js
+++ src/public/components/Gallery.js
@@ -5,11 +5,8 @@ export const Gallery = () => {
     return (
         <div>
             <Navbar />
-            <p>
-                Galerija
-            </p>
             <div className = "images">
-            <img src={require("../../assets/hqdefault.jpg")} alt="no img" width="600px" height="auto"/>
+            <img src={require("../../assets/20190511_185328.jpg")} alt="no img" width="1000px" height="auto"/>
             </div>
         </div>
     )
--- src/public/components/Products.js
+++ src/public/components/Products.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Navbar } from '../../Navbar';
+import '../style/Products.css'
 
 export const Products = () => {
     return (
         <div>
             <Navbar />
-            <p>
-                Proizvodi
-            </p>
+            <div className="product-one">
+                <span>Koren Lincure</span>
+                <img src={require("../../assets/Lincura_sa_Tare.jpg")} alt="no img" className="product-one-img"></img>
+            </div>
+            <div className="product-two">
+                <span>Seme Lincure</span>
+                <img src={require("../../assets/Lincura seme mockup.jpg")} alt="no img" className="product-two-img"></img>
+            </div>
+            <div className="product-three">
+                <span>Rakija sa korenom Lincure</span>
+                <img src={require("../../assets/Lincura rakija.jpg")} alt="no img" className="product-three-img"></img>
+            </div>
         </div>
     )
 }
